98b71b60ca1438bf6f84afc4c7cbba37f6afcde8
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-03-15 23:28:31 -0300

    Removendo dados com delete()

--- src/main/java/com/project/api/control/Control.java
+++ src/main/java/com/project/api/control/Control.java
@@ -1,6 +1,7 @@
 package com.project.api.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,12 @@ public class Control {
     @PutMapping("/api") // é usado para atualizar um recurso existente em um servidor usando uma solicitação HTTP PUT
     public Person edit(@RequestBody Person obj){
         return action.save(obj); // save() pode ser usado para cadastrar e atualizar
+    }
+
+    @DeleteMapping("/api/{id}") // é usado para excluir um recurso existente em um servidor usando uma solicitação HTTP DELETE.
+    public void remove(@PathVariable int id){
+        Person obj = action.findById(id);
+        action.delete(obj);
     }
 
     @GetMapping("/") // é usada para mapear solicitações HTTP GET a métodos de controlador específicos.
