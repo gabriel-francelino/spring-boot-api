7d0306a729c8ac0037c7ffceed2469a9fed0d23c
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-03-17 17:42:48 -0300

    Usando a anotação @Component

--- src/main/java/com/project/api/service/ApiService.java
+++ src/main/java/com/project/api/service/ApiService.java
@@ -1,8 +1,11 @@
 package com.project.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApiService {
     
+    @Autowired
+    private Mensage msg;
 }
